bdd98c0599aa192dd0456e94c45d3c15b0bf3658
nouiouasaad <saadnouioua12@gmail.com>
2023-04-01 00:25:26 +0100

    move app file

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const userRoutes = require("./routes/user.routes");
+const userRoutes = require("./src/routes/user.routes");
+const storeRoutes = require("./src/routes/store.routes");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/stores', storeRoutes);
 
 module.exports = app;
