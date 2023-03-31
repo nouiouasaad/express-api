2ffbfa0fc2a8b74a48cbdad1cba8163d525cd317
nouiouasaad <saadnouioua12@gmail.com>
2023-04-01 00:57:34 +0100

    update port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('./app');
 require('dotenv').config();
 require('./src/config/db.config')
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
