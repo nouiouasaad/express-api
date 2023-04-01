965f93f59142b53f2bdd4e365b084010fd11f5de
nouiouasaad <saadnouioua12@gmail.com>
2023-04-02 00:37:21 +0100

    update the app

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const userRoutes = require("./src/routes/user.routes");
 const storeRoutes = require("./src/routes/store.routes");
 const typeRoutes = require("./src/routes/type.routes");
+const orderRoutes = require("./src/routes/order.routes");
+const productRoutes = require("./src/routes/product.routes");
+const categoryRoutes = require("./src/routes/category.routes");
 
 const app = express();
 
@@ -11,5 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/stores', storeRoutes);
 app.use('/api/v1/types', typeRoutes);
+app.use('/api/v1/orders', orderRoutes);
+app.use('/api/v1/products', productRoutes);
+app.use('/api/v1/categories', categoryRoutes);
 
 module.exports = app;
