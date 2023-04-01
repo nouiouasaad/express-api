95900f3b36d814bea03422133e77f414170a2cdf
nouiouasaad <saadnouioua12@gmail.com>
2023-04-02 00:37:02 +0100

    remove comments

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -31,8 +31,8 @@ const userSchema = new Schema({
 },
     {
         timestamps: {
-            createdAt: 'created_at', // Use `created_at` to store the created date
-            updatedAt: 'updated_at' // and `updated_at` to store the last updated date
+            createdAt: 'created_at',
+            updatedAt: 'updated_at'
         }
     }
 );
