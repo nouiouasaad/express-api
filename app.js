78efb5970757f674cb57e9a5cafc3b1261a6c686
nouiouasaad <saadnouioua12@gmail.com>
2023-04-05 00:46:45 +0100

    add cors configuration

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors')
+
 const userRoutes = require("./src/routes/user.routes");
 const storeRoutes = require("./src/routes/store.routes");
 const typeRoutes = require("./src/routes/type.routes");
@@ -8,6 +10,7 @@ const categoryRoutes = require("./src/routes/category.routes");
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
